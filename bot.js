6615ac7675ef28721068036c5a8c300e6e07ac8d
diskets <prostobogdon@gmail.com>
2018-08-01 14:40:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var prefix = '!';
+
 client.on('ready', () => {
     console.log('Bot started!');
 });
 
 client.on('message', message => {
-    if (message.content === 'test') {
+    if (message.content === prefix +'test') {
     	message.reply('Bot is working!');
   	}
 });
