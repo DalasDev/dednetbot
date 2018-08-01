8bd6ee69fcb7fe9f26cd70cf60d47dece4a0fa15
diskets <prostobogdon@gmail.com>
2018-08-01 18:32:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('message', message => {
 
 let cmd = MessageArray[0];
 
-if(cmd === `${prefix}kick`){
+if(cmd === `kick`){
     
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
