f156b7548c7b20952a80dd28809c080641de19d8
diskets <prostobogdon@gmail.com>
2018-08-02 20:05:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,10 @@ const client = new Discord.Client();
 
 var prefix = '!';
 
+let messageArray = message.content.split(" ");
+let cmd = messageArray[0];
+let args = messageArray.slice(10);
+
 client.on('ready', () => {
     console.log('Bot started!');
 });
@@ -13,37 +17,7 @@ client.on('message', message => {
   	}
 });
 
-//!Kick @Member Why?
 
- let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-
-if(cmd === `kick`){
-    
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kicked In", message.channel)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
-
-    let kickChannel = message.guild.channels.find(`name`, "kick-list");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
-
-    message.guild.member(kUser).kick(kReason);
-    kickChannel.send(kickEmbed);
-}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
