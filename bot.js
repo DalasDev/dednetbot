e5a8879325187c22c93c8e2f11b3866154b0d506
Bogdan Riabokin <prostobogdon@gmail.com>
2018-08-03 13:30:05 +0300

    sdf

--- bot.js
+++ bot.js
@@ -21,11 +21,32 @@ bot.on("message", async message => {
 
   if(cmd === `${prefix}info`){
 
-    message.channel.send({embed: {
-      color: "#4C8BF5",
-      description: "Информация"
-      field: "Ник бота: ${bot.user.username}"
-      field: "Версия: 1.0.0"
+    const embed = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 2048 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
+  message.channel.send({embed});
 }});
 
  }
