7a7c9e982cf6c89fd36ac8bf7063a0b0f475acd8
Bogdan Riabokin <prostobogdon@gmail.com>
2018-08-16 14:04:28 +0300

    sdf

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ bot.on("message", async message => {
       .addField("Канал:", message.channel, true)
       .addField("Время создания жалобы:", message.createdAt, true)
       .addField("Жалоба:", reason, true)
-      .addField("Займитесь этой заявкой!:<@Стражи Порядка>", "", true)
+      .addField("Займитесь этой заявкой!", "!", true)
 
 
   const reportschannel = message.guild.channels.find(`name`, "репорты");
