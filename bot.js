58c3f8bbbd49c288fcdbc8fb52dfd3c1e36195b2
Bogdan Riabokin <prostobogdon@gmail.com>
2018-08-28 13:41:27 +0300

    sdfsda

--- bot.js
+++ bot.js
@@ -17,6 +17,24 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
 
   //-----------------------------------------------------------------------------
+    //!serverinfo
+
+    if (cmd === `${prefix}serverinfo`) {
+      let sicon = message.guild.iconURL;
+      const embed = new Discord.RichEmbed()
+        .setTitle("ИНФОРМАЦИЯ О СЕРВЕРЕ")
+        .setColor("#4C8BF5")
+        .setThumbnail(sicon)
+        .addField("Имя сервера:", message.guild.name, true)
+        .addField("Версия сервера:", "1.9", true)
+        .addField("Сервер создан:", message.guild.createdAt, true)
+        .addField("Вы присоединились:", message.member.joinedAt, true)
+        .addField("Всего учасников:", message.guild.memberCount, true)
+
+    message.channel.send({embed});
+    }
+
+  //-----------------------------------------------------------------------------
     //!botinfo
 
     if(cmd === `${prefix}botinfo`){
