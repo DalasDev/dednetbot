6b2247516cfd2ddbae34df3acbe42f3e69f2a9d8
Bogdan Riabokin <prostobogdon@gmail.com>
2018-08-28 13:51:40 +0300

    FLF

--- bot.js
+++ bot.js
@@ -17,6 +17,35 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
 
   //-----------------------------------------------------------------------------
+    //!report @nick Жалоба
+
+    if (cmd === `${prefix}report`){
+
+      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if(!rUser) return message.channel.send("Пользователь не существует!");
+      let reason = args.join(" ").slice(22);
+
+      let embed = new Discord.RichEmbed()
+        .setTitle("ЖАЛОБА")
+        .setColor("#F76806")
+        .addField("Жалоба на:", `${rUser}`, true)
+        .addField("Жалобу подал:", `${message.author}`, true)
+        .addField("Канал:", message.channel, true)
+        .addField("Время создания жалобы:", message.createdAt, true)
+        .addField("Жалоба:", reason, true)
+
+    const repchannel = message.guild.channels.find(`name`, "repor");
+    const errorschannel = message.guild.channels.find(`name`, "errors");
+    if(!repchannel) return errorschannel.send("Канал жалоб не существует!");
+
+    message.delete().catch(O_o=>{});
+    repchannel.send({embed});
+
+    return;
+    }
+
+
+  //-----------------------------------------------------------------------------
     //!serverinfo
 
     if (cmd === `${prefix}serverinfo`) {
