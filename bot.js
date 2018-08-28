32a29925b90cb8077b1a2ed0a8b95298875fae3f
Bogdan Riabokin <prostobogdon@gmail.com>
2018-08-28 19:35:48 +0300

    seaf

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ bot.on("message", async message => {
            let errorschannel = message.guild.channels.find(`name`, "errors");
            if(!repchannel) return errorschannel.send("Канал отчетов не существует!");
 
-          await kUser.kick();
+           kUser.kick();
 
            message.channel.send(kUser+" был кикнут за "+ kReason);
            repchannel.send({embed});
