36178a582ce0bd12c15a8d8a86e566f902d19d5d
Bogdan Riabokin <prostobogdon@gmail.com>
2018-08-28 13:36:07 +0300

    esrg
    asdfsdaf

--- bot.js
+++ bot.js
@@ -17,13 +17,62 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
 
 
+  //-----------------------------------------------------------------------------
+  //!botinfo
 
+  if(cmd === `${prefix}botinfo`){
+    const embed = new Discord.RichEmbed()
+      .setTitle("ИНФОРМАЦИЯ О БОТЕ")
+      .setColor("#4C8BF5")
+      .addField("Ник бота:", bot.user.username, true)
+      .addField("Версия бота:", "1.0.0", true)
+      .addField("Бот создан:", bot.user.createdAt, true)
+
+  message.channel.send({embed});
+ }
+});
+
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} онлайн!`);
+
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  //-----------------------------------------------------------------------------
+    //!botinfo
+
+    if(cmd === `${prefix}botinfo`){
+      const embed = new Discord.RichEmbed()
+        .setTitle("ИНФОРМАЦИЯ О БОТЕ")
+        .setColor("#4C8BF5")
+        .addField("Ник бота:", bot.user.username, true)
+        .addField("Версия бота:", "1.0.0", true)
+        .addField("Бот создан:", bot.user.createdAt, true)
+
+    message.channel.send({embed});
+   }
+  
 //-----------------------------------------------------------------------------
   //test
 
   if(cmd === `${prefix}test`){
     return message.channel.send("Бот работает!");
   }
+
+//-----------------------------------------------------------------------------
 
 });
 
