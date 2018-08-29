13ccc8c36c953b76311fc49a4ff56a1efebc73eb
Bogdan Riabokin <prostobogdon@gmail.com>
2018-08-29 09:06:25 +0300

    zsdf

--- bot.js
+++ bot.js
@@ -142,9 +142,11 @@ bot.on("message", async message => {
     //!botinfo
 
     if(cmd === `${prefix}botinfo`){
+      let bicon = bot.user.avatarURL;
       const embed = new Discord.RichEmbed()
         .setTitle("ИНФОРМАЦИЯ О БОТЕ")
         .setColor("#4C8BF5")
+        .setThumbnail(bicon)
         .addField("Ник бота:", bot.user.username, true)
         .addField("Версия бота:", "1.0.0", true)
         .addField("Бот создан:", bot.user.createdAt, true)
