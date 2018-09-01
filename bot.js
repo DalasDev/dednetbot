f5b9481029efe8bea571eef770da7ffd274003c5
Alexis Murer <alexis@ottspott.co>
2018-09-01 13:31:31 +0200

    tempmute

--- bot.js
+++ bot.js
@@ -37,38 +37,6 @@ bot.on("message", async message => {
   if(commandfile)
     commandfile.run(bot, message, args);
 
-/*
-  //tempmute @member Time
-
-  if(cmd === `${prefix}tempmute`){
-
-      let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!tomute) return message.reply("Пользователь не существует!");
-      if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Этот пользователь не может быть замучен!");
-      let muterole = message.guild.roles.find(`name`, "Наручники (Мут чата)");
-
-      if(!muterole) return errorschannel.send("Роль мута не найдена!");
-
-      let mutetime = args[1];
-      if(!mutetime) return message.reply("Вы не указали время мута!");
-
-      let repchannel = message.guild.channels.find(`name`, "reports");
-      let errorschannel = message.guild.channels.find(`name`, "errors");
-      if(!repchannel) return errorschannel.send("Канал отчетов не существует!");
-
-      await(tomute.addRole(muterole.id));
-      message.channel.send(`<@${tomute.id}> был замучен  на ${ms(ms(mutetime))}`);
-
-      setTimeout(function(){
-        tomute.removeRole(muterole.id);
-        repchannel.send(`<@${tomute.id}> был размучен!`);
-      }, ms(mutetime));
-
-      return;
-}
-
-*/
-
 });
 
 bot.login(process.env.BOT_TOKEN);
---
+++ commands/tempmute.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+
+//tempmute @member Time
+
+module.exports.run = async (bot, message, args) => {
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!tomute)
+    return message.reply("Пользователь не существует!");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Этот пользователь не может быть замучен!");
+  
+  let muterole = message.guild.roles.find(`name`, "Наручники (Мут чата)");
+
+  if(!muterole)
+    return errorschannel.send("Роль мута не найдена!");
+
+  let mutetime = args[1];
+
+  if(!mutetime)
+    return message.reply("Вы не указали время мута!");
+
+  let repchannel = message.guild.channels.find(`name`, "reports");
+  let errorschannel = message.guild.channels.find(`name`, "errors");
+
+  if(!repchannel)
+    return errorschannel.send("Канал отчетов не существует!");
+
+  await(tomute.addRole(muterole.id));
+  message.channel.send(`<@${tomute.id}> был замучен  на ${ms(ms(mutetime))}`);
+
+  setTimeout(function(){
+    tomute.removeRole(muterole.id);
+    repchannel.send(`<@${tomute.id}> был размучен!`);
+  }, ms(mutetime));
+}
+
+module.exports.help = {
+  name: "tempmute"
+}
