6149a26155b16e3eed22dbc53a0e6452eaf5d2ce
Alexis Murer <alexis@ottspott.co>
2018-09-02 15:36:56 +0200

    unmute

---
+++ commands/unmute.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+
+//tempmute @member Time
+
+module.exports.run = async (bot, message, args) => {
+
+  let tounmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let muterole = message.guild.roles.find(`name`, "Наручники (Мут чата)");
+  let repchannel = message.guild.channels.find(`name`, "reports");
+  let errorschannel = message.guild.channels.find(`name`, "errors");
+
+  if(!tounmute)
+    return message.reply("Пользователь не существует!");
+
+  if(!muterole)
+    return errorschannel.send("Роль мута не найдена!");
+
+  if(!errorschannel)
+    return message.reply("Каналы ошибок не существует!");
+
+  if(!repchannel)
+    return errorschannel.send("Канал отчетов не существует!");
+
+  if(!tomute.roles.has(muterole.id))
+    return message.reply("Пользователь не замучен!");
+
+  await(tounmute.removeRole(muterole.id));
+
+  message.channel.send(`Есть, капитан! <@${tounmute.id}> теперь свободен, как птичка в небе! :ok_hand: `);
+
+}
+
+module.exports.help = {
+  name: "unmute"
+}
