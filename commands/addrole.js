242112dd2a47570d85aed7317d4cc7cb44a9883c
Bogdan Riabokin <prostobogdon@gmail.com>
2018-09-03 12:15:26 +0300

    Addrole

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,24 +3,20 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  console.log("Addrole loaded");
 
-    if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Похоже у тебя недостаточно на это прав, дружище :thinking:.");
-    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.get(args[0]);
-    if(!rMember) return message.reply("Пользователь не существует!");
-    let role = args.join(" ").slice(22);
-    if(!role) return message.reply("Укажите роль!");
-    let gRole = message.guild.roles.find(`name`, role);
-    if(!gRole) return mesage.reply("Роль не существует!");
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Похоже у тебя недостаточно на это прав, дружище :thinking:.");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.reply("Пользователь не существует!");
+  let role = args.join(" ").slice(22);
+  if(!role) return message.reply("Укажите роль!");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.reply("Не могу найти роль!");
 
-    if(rMember.roles.has(gRole.id));
-    await(rMember.addRole(gRole.id));
+  if(rMember.roles.has(gRole.id)) return message.reply("У него уже есть эта роль!");
+  await(rMember.addRole(gRole.id));
 
-    try{
-      rMember.send(`Ты получил роль ${gRole.name}!`)
-    }catch(e){
-    message.channel.send(`<@${rMember.id}> получил роль ${gRole.name}!`)
-  }
+  message.channel.send(`<@${rMember.id}> получил роль ${gRole.name}! :ok_hand:`);
+  
 }
 
 module.exports.help = {
