ab3832f3910320fe17e17dd1085906345bd9d3d1
Alexis Murer <alexis@ottspott.co>
2018-09-03 22:00:43 +0200

    test

--- commands/warn.js
+++ commands/warn.js
@@ -37,60 +37,68 @@ module.exports.run = async (bot, message, args) => {
   });
 
   const warnEmbed = new Discord.RichEmbed()
-  .setTitle("Отчет о варне")
-  .setColor("#fc6400")
-  .addField("Жертва", wUser.tag, true)
-  .addField("Предупреждение выдано в", message.channel, true)
-  .addField("Предупреждений у нарушителя", warns[wUser.id].warns, true)
-  .addField("Причина", reason, true);
+  // .setTitle("Отчет о варне")
+  // .setColor("#fc6400")
+  // .addField("Жертва", wUser.tag, true)
+  // .addField("Предупреждение выдано в", message.channel, true)
+  // .addField("Предупреждений у нарушителя", warns[wUser.id].warns, true)
+  // .addField("Причина", reason, true);
+  .setTitle("ИНФОРМАЦИЯ О СЕРВЕРЕ")
+  .setColor("#4C8BF5")
+  .setThumbnail(sicon)
+  .addField("Имя сервера:", message.guild.name, true)
+  .addField("Версия сервера:", "1.9", true)
+  .addField("Сервер создан:", message.guild.createdAt, true)
+  .addField("Вы присоединились:", message.member.joinedAt, true)
+  .addField("Всего учасников:", message.guild.memberCount, true)
 
   warnchannel.send({warnEmbed});
 
   console.log("message sent to channel");
 
-  if(warns[wUser.id].warns == 2){
-    mutetime = "5m";
-    await(wUser.addRole(muterole.id));
-    message.channel.send(`${wUser.tag} посидит ${mutetime}, подумает...`);
+  // if(warns[wUser.id].warns == 2){
+  //   mutetime = "5m";
+  //   await(wUser.addRole(muterole.id));
+  //   message.channel.send(`${wUser.tag} посидит ${mutetime}, подумает...`);
 
-    setTimeout(function(){
-      wUser.removeRole(muterole.id);
-      warnchannel.reply(`${wUser.tag} был автоматически размучен!`);
-    }, ms(mutetime));
-  }
+  //   setTimeout(function(){
+  //     wUser.removeRole(muterole.id);
+  //     warnchannel.reply(`${wUser.tag} был автоматически размучен!`);
+  //   }, ms(mutetime));
+  // }
 
-  if(warns[wUser.id].warns == 3){
-    mutetime = "15m";
-    await(wUser.addRole(muterole.id));
-    message.channel.send(`${wUser.tag} посидит ${mutetime}, подумает...`);
+  // if(warns[wUser.id].warns == 3){
+  //   mutetime = "15m";
+  //   await(wUser.addRole(muterole.id));
+  //   message.channel.send(`${wUser.tag} посидит ${mutetime}, подумает...`);
 
-    setTimeout(function(){
-      wUser.removeRole(muterole.id);
-      warnchannel.reply(`${wUser.tag} был автоматически размучен!`);
-    }, ms(mutetime));
-  }
+  //   setTimeout(function(){
+  //     wUser.removeRole(muterole.id);
+  //     warnchannel.reply(`${wUser.tag} был автоматически размучен!`);
+  //   }, ms(mutetime));
+  // }
 
-  if(warns[wUser.id].warns == 4){
-    mutetime = "30m";
-    await(wUser.addRole(muterole.id));
-    message.channel.send(`${wUser.tag} посидит ${mutetime}, подумает...`);
+  // if(warns[wUser.id].warns == 4){
+  //   mutetime = "30m";
+  //   await(wUser.addRole(muterole.id));
+  //   message.channel.send(`${wUser.tag} посидит ${mutetime}, подумает...`);
 
-    setTimeout(function(){
-      wUser.removeRole(muterole.id);
-      warnchannel.reply(`${wUser.tag} был автоматически размучен!`);
-    }, ms(mutetime));
-  }
+  //   setTimeout(function(){
+  //     wUser.removeRole(muterole.id);
+  //     warnchannel.reply(`${wUser.tag} был автоматически размучен!`);
+  //   }, ms(mutetime));
+  // }
 
-  if(warns[wUser.id].warns == 5){
-    mutetime = "1h";
-    await(wUser.addRole(muterole.id));
-    message.channel.send(`${wUser.tag} посидит ${mutetime}, подумает...`);
+  // if(warns[wUser.id].warns == 5){
+  //   mutetime = "1h";
+  //   await(wUser.addRole(muterole.id));
+  //   message.channel.send(`${wUser.tag} посидит ${mutetime}, подумает...`);
 
-    setTimeout(function(){
-      wUser.removeRole(muterole.id);
-      warnchannel.reply(`${wUser.tag} был автоматически размучен!`);
-    }, ms(mutetime));
-  }
+  //   setTimeout(function(){
+  //     wUser.removeRole(muterole.id);
+  //     warnchannel.reply(`${wUser.tag} был автоматически размучен!`);
+  //   }, ms(mutetime));
+  // }
 }
 
 module.exports.help = {
