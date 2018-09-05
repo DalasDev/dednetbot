d7f457fdd8f9a9890e81e44487c04e70730d5cf8
Alexis Murer <alexis@ottspott.co>
2018-09-06 01:42:07 +0200

    bootstrap added

--- public/index.js
+++ public/index.js
@@ -1,13 +1,4 @@
-// const botconfig = require("./botconfig.json");
-// const Discord = require("discord.js");
-const fs = require("fs");
-// const bot = new Discord.Client({disableEveryone: true});
-// const express = require('express');
-// const app = express();
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
-
 function warnings() {
-	console.log("Warns: " + warns);
-	let to_show = JSON.stringify(warns);
-	document.getElementById("warns").innerHTML = to_show;
+	let warn_data = JSON.parse("./warnings.json");
+	document.getElementById("warns").innerHTML = warn_data;
 }
