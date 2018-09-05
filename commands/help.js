c3d002f12ea7afad42b48a41b9fbd787c2a4dd32
Alexis Murer <alexis@ottspott.co>
2018-09-05 21:44:06 +0200

    help

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,26 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+	let bicon = bot.user.avatarURL;
+	const embed = new Discord.RichEmbed()
+	.setTitle("Доступные на данный момент комманды")
+	.setColor("#BF44FF")
+	.setThumbnail(bicon)
+	.addField("!addrole роль", "Добавляет роль, доступна админу", true)
+	.addField("!ban", "Тут и ежу ясно", true)
+	.addField("!botinfo", "Информация о боте (можем написать что захотим)", true)
+	.addField("!kick", "И тут все понятно", true)
+	.addField("!removerole роль", "Удаляет роль, доступна админу", true)
+	.addField("!report user причина", "Отправляет жалобу в скрытый канал", true)
+	.addField("!serverinfo", "Информация о сервере, можем показывать что захотим но солгать в цифрах не сможем", true)
+	.addField("!tempmute user Xs/m/h/d", "Мут на X времени", true)
+	.addField("!unmute", "Тут все логично...", true)
+	.addField("!voicemute user Xs/m/h/d", "Мут микрофона на X времени", true)
+	.addField("!voiceunmute", "Анмут микрофона преждевременно", true)
+	.addField("!warn user reason", "Варны, сами опробуете, накопительная система", true)
+	.addField("Продолжение следует...", true)
+
+	message.channel.send({embed});
 }
 
 
