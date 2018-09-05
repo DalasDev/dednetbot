928587fcc18444500568ebfed62ce1d8fd990443
Alexis Murer <alexis@ottspott.co>
2018-09-06 01:57:58 +0200

    bootstrap added

--- public/index.js
+++ public/index.js
@@ -1,4 +1,6 @@
+let warns = JSON.parse("https://retrobotproject.herokuapp.com/warnings.json", "utf8");
+
 function warnings() {
-	let warn_data = JSON.parse("warnings.json");
-	document.getElementById("warns").innerHTML = warn_data;
+	let data = JSON.parse(warns);
+	document.getElementById("warns").innerHTML = data;
 }
