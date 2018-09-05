3d6403aa4311246de47ff42f967d6314580bc904
Alexis Murer <alexis@ottspott.co>
2018-09-05 23:42:34 +0200

    test

--- commands/help.js
+++ commands/help.js
@@ -22,7 +22,7 @@ module.exports.run = async (bot, message, args) => {
 	.addField("!voiceunmute", "Анмут микрофона преждевременно", true)
 	.addField("!warn user reason", "Варны, сами опробуете, накопительная система", true)
 	.setFooter("Продолжение следует...", "")
-	.setImage(process.env + "/images/easy.gif")
+	.setImage("https://retrobotproject.herokuapp.com/images/easy.gif")
 
 	message.channel.send({embed});
 }
