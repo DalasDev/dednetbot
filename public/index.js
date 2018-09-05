de2e9f8d2d40251a45442e5b79de10c46648a389
Alexis Murer <alexis@ottspott.co>
2018-09-06 01:33:34 +0200

    bootstrap added

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 // const botconfig = require("./botconfig.json");
 // const Discord = require("discord.js");
-// const fs = require("fs");
+const fs = require("fs");
 // const bot = new Discord.Client({disableEveryone: true});
 // const express = require('express');
 // const app = express();
