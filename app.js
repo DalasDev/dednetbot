91c30914f3a6a2e5da999af8083a0c9f204672cd
Alexis Murer <alexis@ottspott.co>
2018-09-05 23:19:33 +0200

    express

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+const express = require('express');
+const app = express();
 bot.commands = new Discord.Collection();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 8080, () => 
+  console.log("All is set")
+)
 
 fs.readdir("./commands/", (err, files) => {
   if (err)
