f1846ced6ef96295320695b58ac8df636c43747a
Alexis Murer <alexis@ottspott.co>
2018-09-06 01:59:43 +0200

    bootstrap added

--- public/index.js
+++ public/index.js
@@ -1,6 +1,5 @@
-let warns = JSON.parse("https://retrobotproject.herokuapp.com/warnings.json", "utf8");
-
 function warnings() {
+	let warns = JSON.parse("https://retrobotproject.herokuapp.com/warnings.json", "utf8");
 	let data = JSON.parse(warns);
 	document.getElementById("warns").innerHTML = data;
 }
