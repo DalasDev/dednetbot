9202baed3f675b2966659c80da0f009e9a45bca0
Alexis Murer <alexis@ottspott.co>
2018-09-06 01:07:51 +0200

    bootstrap added

--- public/index.js
+++ public/index.js
@@ -4,3 +4,10 @@ const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 const express = require('express');
 const app = express();
+
+var para = document.createElement("p");
+var node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+var element = document.getElementById("div1");
+element.appendChild(para);
