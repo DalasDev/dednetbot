2d0dfccba18576346bb96005ddd9e1ac0599c603
Alexis Murer <alexis@ottspott.co>
2018-09-06 22:15:25 +0200

    fix

--- commands/sasnibi4ara.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.channel.send("so what we gonna do now??");
-}
-
-module.exports.help = {
-    name: "sasnibi4ara"
-}
