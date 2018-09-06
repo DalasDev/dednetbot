1380764eedc82279aebb9828895dfc243193df20
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-06 21:58:55 +0300

    Fix

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Похоже у тебя недостаточно на это прав, дружище :thinking:.");
   if(!args[0]) return message.channel.send("Ты не написал сколько сообщений удалить!");
   if(isNaN(args[0])){
-    message.channel.send("Укажи число а не что попало!");
+    message.channel.send(`Укажи число а не какую-то хрень!`).then(msg => msg.delete(4000));
   }
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Удалено ${args[0]} сообщений. :ok_hand:`).then(msg => msg.delete(4000));
