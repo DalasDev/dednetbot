c6d15d96f9db8f4ec54cd8f3210f93ede5b6585a
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-06 13:55:39 +0300

    Site

--- public/index.js
+++
@@ -1,9 +0,0 @@
-function warnings() {
-	var request = new XMLHttpRequest();
-	request.open("GET", "./warnings.json", false);
-	request.send(null)
-	var JSONobj = JSON.parse(request.responseText);
-
-	let data = JSON.parse(JSONobj);
-	document.getElementById("warns").innerHTML = data;
-}
