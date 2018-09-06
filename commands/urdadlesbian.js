0f00a74d14cb01208e7045d4d3ad2fa2f630283e
Alexis Murer <alexis@ottspott.co>
2018-09-06 22:06:24 +0200

    fix

---
+++ commands/urdadlesbian.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send("dont try to do dat");
+}
+
+module.exports.help = {
+    name: "urdadlesbian"
+}
