93877cb5ed3f9cb0b04332c7f46c4414a48b2389
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-06 21:09:30 +0300

    msg time

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Похоже у тебя недостаточно на это прав, дружище :thinking:.");
   if(!args[0]) return message.channel.send("Ты не написал что мне сказать!");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Удалено ${args[0]} сообщений. :ok_hand:`).then(msg => msg.delete(2000));
+  message.channel.send(`Удалено ${args[0]} сообщений. :ok_hand:`).then(msg => msg.delete(4000));
 });
 
 }
