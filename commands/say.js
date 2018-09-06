23410866977cb33e9ae3b92260aec412a1730c02
Alexis Murer <alexis@ottspott.co>
2018-09-06 20:05:36 +0200

    fix

--- commands/say.js
+++ commands/say.js
@@ -4,11 +4,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
       let mainchannel = message.guild.channels.find(`name`, "general");
-      if(!message.member.roles.some(r=>["Разраб"].includes(r.name)) return;
+      if(!message.member.roles.some(r=>["Разраб"].includes(r.name)))
+       return;
       const sayMessage = args.join(" ");
       message.delete().catch();
       mainchannel.send(sayMessage);
-
 }
 
 module.exports.help = {
