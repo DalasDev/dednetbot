5a8cfdb6184e17ef5ba4d2243563aee6d9349fe4
Alexis Murer <alexis@ottspott.co>
2018-09-06 21:13:01 +0200

    fix

--- commands/clear.js
+++ commands/clear.js
@@ -6,15 +6,15 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_ROLES"))
     return message.reply("похоже у тебя недостаточно на это прав, дружище :thinking:.");
   if(!args[0])
-    return message.channel.send("ты не написал сколько сообщений удалить!");
+    return message.channel.send("Ты не написал сколько сообщений удалить!");
   if(isNaN(args[0])){
-    message.channel.send(`укажи число а не какую-то хрень!`).then(msg => msg.delete(4000));
+    message.channel.send(`Укажи число а не какую-то хрень!`).then(msg => msg.delete(4000));
   }
   // if(args[0] < 2 || args[0] > 100){
   //   message.channel.send(`укажи число в пределах от 2 до 100`).then(msg => msg.delete(4000));
   // }
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`удалено ${args[0]} сообщений. :ok_hand:`).then(msg => msg.delete(4000));
+  message.channel.send(`Удалено ${args[0]} сообщений. :ok_hand:`).then(msg => msg.delete(4000));
 });
 
 }
