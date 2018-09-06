ff9b6c1b092effe4edef125c4f96cbc07a01ca91
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-06 21:54:38 +0300

    fix

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Похоже у тебя недостаточно на это прав, дружище :thinking:.");
-  if(!args[0]) return message.channel.send("Ты не написал что мне сказать!");
+  if(!args[0]) return message.channel.send("Ты не написал сколько сообщений удалить!");
+  if(isNaN(args[0])){
+    message.channel.send("Укажи число а не что попало!");
+  }
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Удалено ${args[0]} сообщений. :ok_hand:`).then(msg => msg.delete(4000));
 });
