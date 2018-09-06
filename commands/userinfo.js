852a50d8ecc9d5103f0f7a7c66d1632bf2340f8c
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-07 01:35:21 +0300

    email

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -13,6 +13,8 @@ let iUser = message.guild.member(message.mentions.users.first()) || message.guil
   .addField("ID пользователя:", `${iUser.id}`, true)
   .addField("Присоединился:", `${iUser.joinedAt}`, true)
   .addField("Зарегистрировался:", `${iUser.createdAt}`, true)
+  .addField("E-mail аккаунта:", `${iUser.email}`, true)
+
 
   message.channel.send({embed});
 }
