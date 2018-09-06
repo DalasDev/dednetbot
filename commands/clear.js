81d8bb4c1ef42f33a4826b07329caa79aabf30f5
Alexis Murer <alexis@ottspott.co>
2018-09-06 22:36:01 +0200

    fix

--- commands/clear.js
+++ commands/clear.js
@@ -10,9 +10,9 @@ module.exports.run = async (bot, message, args) => {
   if(isNaN(args[0])){
     message.channel.send(`Укажи число а не какую-то хрень!`).then(msg => msg.delete(4000));
   }
-  // if(args[0] < 2 || args[0] > 100){
-  //   message.channel.send(`укажи число в пределах от 2 до 100`).then(msg => msg.delete(4000));
-  // }
+  if(args[0] < 2){
+    message.channel.send(`укажи число в пределах от 2 до 100`).then(msg => msg.delete(4000));
+  }
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Удалено ${args[0]} сообщений. :ok_hand:`).then(msg => msg.delete(4000));
 });
