f2219f8dd7c571f31a82986c0353311a82a11204
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-07 01:13:08 +0300

    userinfo

---
+++ commands/userinfo.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let iUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+
+  let avatar = iUser.iconURL;
+  const embed = new Discord.RichEmbed()
+  .setTitle("ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ")
+  .setColor("#4C8BF5")
+  .setThumbnail(avatar)
+  .addField("Ник пользователя:", `${iUser}`, true)
+  .addField("ID пользователя:", `${iUser.id}`, true)
+  .addField("Присоединился:", `${iUser.joinedAt}`, true)
+  .addField("Зарегистрировался:", `${iUser.createdAt}`, true)
+
+module.exports.help = {
+  name: "userinfo"
+}
