c0a436727315cbbb23c51c8b06d4a8f734b99285
Alexis Murer <alexis@ottspott.co>
2018-09-06 02:11:47 +0200

    test

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 function warnings() {
 	var request = new XMLHttpRequest();
-	request.open("GET", "warnings.json", false);
+	request.open("GET", "./warnings.json", false);
 	request.send(null)
 	var JSONobj = JSON.parse(request.responseText);
 
