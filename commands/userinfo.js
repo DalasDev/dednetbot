3585241603fdf962d944af3d07597b0bb4d49c10
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-07 02:14:36 +0300

    fix

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -11,10 +11,10 @@ let iUser = message.guild.member(message.mentions.users.first()) || message.guil
   .setThumbnail(avatar)
   .addField("Ник пользователя:", `${iUser}`, true)
   .addField("ID пользователя:", `${iUser.id}`, true)
+  .addField("Дискорд Тег:", iUser.user.tag, true);
   .addField("Присоединился:", `${iUser.joinedAt}`, true)
   .addField("Зарегистрировался:", iUser.user.createdAt, true)
-  .addField("Дискорд Тег:", iUser.user.tag, true);
-
+  .addField("Зарегистрировался:", iUser.user.lastMessage, true)
 
   message.channel.send({embed});
 }
