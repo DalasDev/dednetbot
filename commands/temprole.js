9c0c55ee1181aa69e30783aa8db3c23655a57c8c
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-07 13:08:40 +0300

    fix

--- commands/temprole.js
+++ commands/temprole.js
@@ -8,11 +8,11 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Похоже у тебя недостаточно на это прав, дружище :thinking:.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Пользователь не существует!");
-  let role = args[2];
+  let role = args[1];
   if(!role) return message.reply("Укажите роль!");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("Не могу найти роль!");
-  let roletime = args[3];
+  let roletime = args[2];
   if(!roletime) return message.reply("Укажите время");
 
   let repchannel = message.guild.channels.find(`name`, "reports");
