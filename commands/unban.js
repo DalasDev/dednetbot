e1adf5447a07a52ea0e8b1bfbac2feff450d9f1c
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-07 03:42:40 +0300

    unban

---
+++ commands/unban.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+//unban @member
+
+module.exports.run = async (bot, message, args) => {
+
+  let tounban = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let repchannel = message.guild.channels.find(`name`, "reports");
+  let errorschannel = message.guild.channels.find(`name`, "errors");
+
+  if(!message.member.hasPermission("BAN_MEMBERS", "ADMINISTRATOR"))
+    return message.channel.send("Похоже у тебя недостаточно на это прав, дружище :thinking:. ");
+
+  if(!tounmute)
+    return message.reply("Пользователь не существует!");
+
+  if(!errorschannel)
+    return message.reply("Каналы ошибок не существует!");
+
+  if(!repchannel)
+    return errorschannel.send("Канал отчетов не существует!");
+
+  repchannel.send(`<@${tounmute.id}> был разбанен администратором!`);
+
+  message.guild.unban(tounban);
+
+  message.channel.send(`Есть, капитан! <@${tounmute.id}> теперь снова получил визу в наш город! :ok_hand: `);
+
+}
+
+module.exports.help = {
+  name: "unban"
+}
