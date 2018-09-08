ab0ec617a80adfa7b40a41474e10c2ebcebd6d33
Alexis Murer <alexis@ottspott.co>
2018-09-08 15:23:33 +0200

    bothelp

--- commands/bothelp.js
+++ commands/bothelp.js
@@ -15,16 +15,16 @@ module.exports.run = async (bot, message, args) => {
 	.setThumbnail(bicon)
 	.addField("!addrole роль", "Добавляет роль, доступна админу", true)
 	.addField("!ban", "Тут и ежу ясно", true)
-	.addField("!botinfo", "Информация о боте", true)
+	.addField("!botinfo", "Информация о боте (можем написать что захотим)", true)
 	.addField("!kick", "И тут все понятно", true)
 	.addField("!removerole роль", "Удаляет роль, доступна админу", true)
 	.addField("!report user причина", "Отправляет жалобу в скрытый канал", true)
-	.addField("!serverinfo", "Информация о сервере", true)
+	.addField("!serverinfo", "Информация о сервере, можем показывать что захотим но солгать в цифрах не сможем", true)
 	.addField("!tempmute user Xs/m/h/d", "Мут на X времени", true)
 	.addField("!unmute", "Тут все логично...", true)
 	.addField("!voicemute user Xs/m/h/d", "Мут микрофона на X времени", true)
 	.addField("!voiceunmute", "Анмут микрофона преждевременно", true)
-	.addField("!warn user reason", "Варны, накопительная система", true)
+	.addField("!warn user reason", "Варны, сами опробуете, накопительная система", true)
 	.addField("!unban", "Попробуйте сами угадать", true)
 	.addField("!clear X", "Удалить последние Х сообщений с чата (от 2 до 100)", true)
 	.setFooter("Продолжение следует...", "")
@@ -35,5 +35,5 @@ module.exports.run = async (bot, message, args) => {
 
 
 module.exports.help = {
-	name: "help"
+	name: "bothelp"
 }
