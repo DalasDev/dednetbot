45d07f4d3d4282453f8795241ce4782971c6b4ba
Alexis Murer <alexis@ottspott.co>
2018-09-08 17:54:43 +0200

    d

--- commands/roll.js
+++ commands/roll.js
@@ -20,8 +20,8 @@ module.exports.run = async (bot, message, args) => {
 		var separator = "-";
 		var arrayOfNumbers = args[0].split(separator);
 		console.log("Min is " + arrayOfNumbers[0] + " and Max is " + arrayOfNumbers[1]);
-		if (arrayOfNumbers[1] > arrayOfNumbers[0]){
-			var result = Math.floor(Math.random() * (arrayOfNumbers[1] - arrayOfNumbers[0] + 1)) + arrayOfNumbers[0];
+		if (arrayOfNumbers[1] > arrayOfNumbers[0] && arrayOfNumbers[0] = "1"){
+			var result = Math.floor(Math.random() * arrayOfNumbers[1]) + 1;
 			return message.channel.send("Крутанул 🎲 и выпало " + result);
 		}
 		else if (arrayOfNumbers[1] = arrayOfNumbers[0]){
