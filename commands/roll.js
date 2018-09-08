930214627bbebad70e6afe0a07722a3d07eb14a3
Alexis Murer <alexis@ottspott.co>
2018-09-08 10:52:01 +0200

    test

--- commands/roll.js
+++ commands/roll.js
@@ -5,8 +5,15 @@ module.exports.run = async (bot, message, args) => {
 	if(!message.member.hasPermission("BAN_MEMBERS", "ADMINISTRATOR"))
 		return message.channel.send("Похоже у тебя недостаточно на это прав, дружище :thinking:.");
 
-	else{
+	if (!args){
+		return message.channel.send("Укажите диапазон, к примеру 1-12");
+	}
+
+	else if(!args[1]){
 		return message.channel.send(args);
+	}
+	else{
+		return message.channel.send("Найдено больше одного аргумента");
 	}
 }
 
