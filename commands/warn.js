e78e0e93e29a63166a87f880f1a7b98f99e9fc54
Alexis Murer <alexis@ottspott.co>
2018-09-09 18:20:08 +0200

    mongoose

--- commands/warn.js
+++ commands/warn.js
@@ -52,7 +52,7 @@ module.exports.run = async (bot, message, args) => {
   warns[wUser.id].warns++;
 
   //mongoose test
-
+  
   console.log("mongoose test 1");
   var warnUser = mongoose.model("warnUser", warnSchema);
   let id = `<@${wUser.id}>`;
@@ -63,11 +63,9 @@ module.exports.run = async (bot, message, args) => {
   myData.save()
   .then(item => {
     console.log("mongoose test 2");
-    res.send("item saved to database");
   })
   .catch(err => {
     console.log("mongoose test 3");
-    res.status(400).send("unable to save to database");
   });
 
   //end of mongoose test
