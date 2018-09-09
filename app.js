8d8688f149b87bb7553ad0297d65f83fbcdb70ad
Alexis Murer <alexis@ottspott.co>
2018-09-09 10:43:04 +0200

    test

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ bot.on("message", async message => {
 
     if(commandfile){
       console.log("[app.js] Command to execute: " + cmd);
-      console.log("message.member is : " + JSON.stringify(message.member));
+      console.log("message.member is : " + message.member);
       commandfile.run(bot, message, args);
     }
   }
