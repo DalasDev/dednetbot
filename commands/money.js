278ef47d132676848e1495fd9f3ea37f10b8ca44
Alexis Murer <alexis@ottspott.co>
2018-09-11 05:13:06 +0200

    scanuser

--- commands/money.js
+++ commands/money.js
@@ -6,26 +6,50 @@ mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@
 var User = require('./../schemas/user_model.js');
 
 module.exports.run = async (bot, message, args) => {
-	var user_obj = User.findOne({
-		userID: message.member.id 
-	}, function (err, foundObj) {
-		if (err)
-			console.log("Error on database findOne: " + err);
-		else {
-			var avatar = message.member.user.avatarURL;
-			var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
-			const embed = new Discord.RichEmbed()
-			.setTitle("Личный счет " + message.member.displayName)
-			.setColor("#0000FF")
-			.addField("Наличкой", foundObj.retrocoinCash + " ⓟ (ретриков)", true)
-			.addField("В банке", foundObj.retrocoinBank + " ⓟ (ретриков)", true)
-//			.addField("Всего", total + " ⓟ (ретриков)", true)
-			.setThumbnail(avatar)
-//			.setImage("https://retrobotproject.herokuapp.com/images/bender.gif")
+	
+	let toScan = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-			message.channel.send({embed});
-		}
-	});
+	if(!toScan){
+		var user_obj = User.findOne({
+			userID: message.member.id 
+		}, function (err, foundObj) {
+			if (err)
+				console.log("Error on database findOne: " + err);
+			else {
+				var avatar = message.member.user.avatarURL;
+				var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
+				const embed = new Discord.RichEmbed()
+				.setTitle("Личный счет " + message.member.displayName)
+				.setColor("#0000FF")
+				.addField("Наличкой", foundObj.retrocoinCash + " ⓟ (ретриков)", true)
+				.addField("В банке", foundObj.retrocoinBank + " ⓟ (ретриков)", true)
+	//			.addField("Всего", total + " ⓟ (ретриков)", true)
+				.setThumbnail(avatar)
+
+				message.channel.send({embed});
+			}
+		});
+	}else{
+		var user_obj = User.findOne({
+			userID: toScan.id 
+		}, function (err, foundObj) {
+			if (err)
+				console.log("Error on database findOne: " + err);
+			else {
+				var avatar = toScan.user.avatarURL;
+				var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
+				const embed = new Discord.RichEmbed()
+				.setTitle("Личный счет " + toScan.displayName)
+				.setColor("#0000FF")
+				.addField("Наличкой", foundObj.retrocoinCash + " ⓟ (ретриков)", true)
+				.addField("В банке", foundObj.retrocoinBank + " ⓟ (ретриков)", true)
+	//			.addField("Всего", total + " ⓟ (ретриков)", true)
+				.setThumbnail(avatar)
+
+				message.channel.send({embed});
+			}
+		});
+	}
 }
 
 
