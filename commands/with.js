44e6b7a6e4ecee377d1d16e39478937b033f2d09
Alexis Murer <alexis@ottspott.co>
2018-09-11 20:46:26 +0200

    with

--- commands/with.js
+++ commands/with.js
@@ -5,9 +5,13 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@ds239071.mlab.com:39071/retrobotdb");
 var User = require('./../schemas/user_model.js');
 
+function isNumeric(value) {
+	return /^\d+$/.test(value);
+}
+
 module.exports.run = async (bot, message, args) => {
-	
-	if (args[1] == "all"){
+
+	if (args[0] === "all"){
 		var user_obj = User.findOne({
 			userID: message.member.id 
 		}, function (err, foundObj) {
@@ -30,14 +34,53 @@ module.exports.run = async (bot, message, args) => {
 
 					var avatar = message.member.user.avatarURL;
 					var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
+
 					const embed = new Discord.RichEmbed()
-					.setTitle("Личный счет " + message.member.displayName)
+					.setTitle("Все ⓟ (ретрики) сняты с банковского счета! Новый баланс " + message.member.displayName)
 					.setColor("#0000FF")
 					.addField("Наличкой", foundObj.retrocoinCash + " ⓟ (ретриков)", true)
 					.addField("В банке", foundObj.retrocoinBank + " ⓟ (ретриков)", true)
-					.setThumbnail(avatar)
 
 					message.channel.send({embed});
+				}
+			}
+		});
+	}
+	else if (isNumeric(args[0])){
+		var user_obj = User.findOne({
+			userID: message.member.id 
+		}, function (err, foundObj) {
+			if (err)
+				console.log("Error on database findOne: " + err);
+			else {
+				if (!foundObj)
+					console.log("Something stange happend");
+				else {
+					var actBank = foundObj.retrocoinBank;
+					var actCash = foundObj.retrocoinCash;
+					var toWith = Number(args[0]);
+					var newBank = actBank - toWith;
+					var newCash = actCash + toWith;
+					if (newBank >= 0){
+						foundObj.retrocoinCash = newCash;
+						foundObj.retrocoinBank = newBank;
+						foundObj.save(function(err, updatedObj){
+							if(err)
+								console.log(err);
+						})
+						var avatar = message.member.user.avatarURL;
+						var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
+						const embed = new Discord.RichEmbed()
+						.setTitle(toWith + " ретриков снято со счета! Новый баланс " + message.member.displayName)
+						.setColor("#0000FF")
+						.addField("Наличкой", foundObj.retrocoinCash + " ⓟ (ретриков)", true)
+						.addField("В банке", foundObj.retrocoinBank + " ⓟ (ретриков)", true)
+
+						message.channel.send({embed});
+					}
+					else {
+						return message.channel.send("У тебя разве хватает ⓟ (ретриков) на такое действие? :thinking:");
+					}
 				}
 			}
 		});
