48be3a4ffd079105271559988d817b2f36db6a2f
Alexis Murer <alexis@ottspott.co>
2018-09-14 23:37:40 +0200

    fix

--- commands/work.js
+++ commands/work.js
@@ -4,8 +4,6 @@ const ms = require("ms");
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@ds239071.mlab.com:39071/retrobotdb");
 var User = require('./../schemas/user_model.js');
-var bot = new Discord.Client({disableEveryone: true});
-var retricIcon = bot.emojis.get("490246096539615232");
 
 
 function isNumeric(value) {
@@ -19,6 +17,8 @@ function random(min, max) {
 
 module.exports.run = async (bot, message, args) => {
 
+	var retricIcon = bot.emojis.find("name", "retric");
+	
 	var user_obj = User.findOne({
 		userID: message.member.id
 	}, function (err, foundObj) {
