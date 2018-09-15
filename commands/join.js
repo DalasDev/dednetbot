9068b6fcdb9ceea53ba0db89197be9a5a9c694c9
Alexis Murer <alexis@ottspott.co>
2018-09-15 16:39:22 +0200

    join test

--- commands/join.js
+++ commands/join.js
@@ -2,14 +2,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+	//лимит который нужно прописать во все комманды что бы никто другой пока что не использовал
 	if(!message.member.hasPermission("MANAGE_ROLES"))
 		return;
 	
-	const voiceChannel = message.member.voiceChannel;
+	const voiceChannel = msg.member.voiceChannel;
 			
 	if (!voiceChannel || voiceChannel.type !== 'voice')
-		return message.reply("Что-то пошло не так, не могу прыгнуть в твой канал...");
-	voiceChannel.join();
+		return msg.reply('I couldn\'t connect to your voice channel...');
+	voiceChannel.join().then(connection => console.log('Connected')).catch(console.log(err));
 }
 
 module.exports.help = {
