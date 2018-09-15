0035a111162698b9e40f50d1d74de0f7ba2a7d4a
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-15 13:43:25 +0300

    fix

--- commands/voicemute.js
+++ commands/voicemute.js
@@ -6,8 +6,8 @@ const ms = require("ms");
 module.exports.run = async (bot, message, args) => {
 
   let tovmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  let vmutetime = message.guild.members.get(args[2]);
-  let vmreason = args.join(" ").slice(22);
+  let vmutetime = message.guild.members.get(args[1]);
+  let vmreason = message.guild.members.get(args[2]);
   let repchannel = message.guild.channels.find(`name`, "🌘reports_bots");
   let errorschannel = message.guild.channels.find(`name`, "🌏errors_bots");
 
