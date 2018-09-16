e1a2fd2d6dddd9151abe84e4019fec6b09ef3da3
Alexis Murer <alexis@ottspott.co>
2018-09-16 09:59:20 +0200

    test

--- commands/bal.js
+++ commands/bal.js
@@ -24,12 +24,11 @@ module.exports.run = async (bot, message, args) => {
 			else {
 				var avatar = message.member.user.avatarURL;
 				var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
-				var c = numberWithCommas(foundObj.retrocoinCash);
 				const embed = new Discord.RichEmbed()
 				.setTitle("Личный счет " + message.member.displayName)
 				.setColor("#0000FF")
-				.addField("Наличкой", `${c} ${retricIcon}`, true)
-				.addField("В банке", `${foundObj.retrocoinBank} ${retricIcon}`, true)
+				.addField("Наличкой", `${numberWithCommas(foundObj.retrocoinCash)} ${retricIcon}`, true)
+				.addField("В банке", `${numberWithCommas(foundObj.retrocoinBank)} ${retricIcon}`, true)
 				.setThumbnail(avatar)
 
 				message.channel.send({embed});
@@ -47,8 +46,8 @@ module.exports.run = async (bot, message, args) => {
 				const embed = new Discord.RichEmbed()
 				.setTitle("Личный счет " + toScan.displayName)
 				.setColor("#0000FF")
-				.addField("Наличкой", `${foundObj.retrocoinCash} ${retricIcon}`, true)
-				.addField("В банке", `${foundObj.retrocoinBank} ${retricIcon}`, true)
+				.addField("Наличкой", `${numberWithCommas(foundObj.retrocoinCash)} ${retricIcon}`, true)
+				.addField("В банке", `${numberWithCommas(foundObj.retrocoinBank)} ${retricIcon}`, true)
 				.setThumbnail(avatar)
 
 				message.channel.send({embed});
