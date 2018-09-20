6e77273ee336e10298195031ac79151b06b4ec49
Alexis Murer <alexis@ottspott.co>
2018-09-20 21:12:40 +0200

    userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -37,9 +37,11 @@ module.exports.run = async (bot, message, args) => {
       description: "__не установлен__",
       fields: [{
         name: `***Личный баланс :*** ${retricIcon}`
+        value: `***Нарушений :*** овер миллион`
       },
       {
-        value: "```тест:test\nтест2:test2```"
+        name: "```тест:test\nтест2:test2```"
+        value: "```тест3:test3\nтест4:test4```"
       }
       ],
       timestamp: new Date(),
