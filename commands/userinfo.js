086a0ad6c793be2fc5734ab4dbcb0331040aa642
Alexis Murer <alexis@ottspott.co>
2018-09-20 21:23:56 +0200

    userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -26,6 +26,10 @@ module.exports.run = async (bot, message, args) => {
 
     message.channel.send({embed: {
       color: 3447003,
+      author: {
+        name: `**Retro Valley** ${dabIcon} ${iUser}`,
+        icon_url: message.guild.iconURL
+      },
       image:{
         url: `${iUser.user.avatarURL}`
       },
