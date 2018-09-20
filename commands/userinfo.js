3e903be28ded4444f7cad44170e83f31338be803
Alexis Murer <alexis@ottspott.co>
2018-09-21 00:54:09 +0200

    userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,4 +1,9 @@
 const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@ds239071.mlab.com:39071/retrobotdb");
+var User = require('./../schemas/user_model.js');
 
 module.exports.run = async (bot, message, args) => {
 
