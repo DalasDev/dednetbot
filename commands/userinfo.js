3a8d26ebd6f4801fb5da1a54ca36718832710352
Alexis Murer <alexis@ottspott.co>
2018-09-21 00:11:14 +0200

    userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -9,50 +9,38 @@ module.exports.run = async (bot, message, args) => {
   if (!iUser)
     return message.reply("пользователь не найден / не указан!");
 
-  let avatar = iUser.user.avatarURL;
-  var dabIcon = bot.emojis.find("name", "dab");
   var retricIcon = bot.emojis.find("name", "retric");
   var hmmIcon = bot.emojis.find("name", "hmm");
-  // const embed = new Discord.RichEmbed()
-  // .setTitle("ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ")
-  // .setColor("#4C8BF5")
-  // .setThumbnail(avatar)
-  // .addField("Ник пользователя:", `${iUser}`, true)
-  // .addField("ID пользователя:", `${iUser.id}`, true)
-  // .addField("Присоединился:", `${iUser.joinedAt}`, true)
-  // .addField("Зарегистрировался:", iUser.user.createdAt, true)
 
-  // message.channel.send({embed});
-
-    message.channel.send({embed: {
-      color: 3447003,
-      icon_url: message.guild.iconURL,
-      title: `***Retro Valley*** :zap: ${iUser.displayName}`,
-      description: `**Высшая роль:** ${iUser.highestRole.name}`,
-      fields: [
-        {
-          name: `***Личный статус*** :speech_left:`,
-          value: `__не установлен__`
-        },
-        {
-          name: `***Личный баланс :*** ${retricIcon}`,
-          value: `***Нарушений :*** овер миллион`
-        },
-        {
-          name: "```тест:test\nтест2:test2```",
-          value: "```тест3:test3\nтест4:test4```"
-        }
-      ],
-      timestamp: new Date(),
-      footer: {
-        icon_url: message.author.avatarURL,
-        text: `© ${message.member.displayName}`
-      },
-      thumbnail: {
-        url: `${iUser.user.avatarURL}`
-      }
+  message.channel.send({embed: {
+    color: 3447003,
+    icon_url: message.guild.iconURL,
+    title: `***Retro Valley*** :zap: ${iUser.displayName}`,
+    description: `(**высшая роль:** ${iUser.highestRole.name})`,
+    fields: [
+    {
+      name: `***Личный статус*** :speech_left:`,
+      value: `__не установлен__`
+    },
+    {
+      name: `***Личный баланс :*** ${retricIcon}`,
+      value: `***Нарушений :*** овер миллион`
+    },
+    {
+      name: "```тест:test\nтест2:test2```",
+      value: "```тест3:test3\nтест4:test4```"
     }
-  });
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: message.author.avatarURL,
+      text: `© ${message.member.displayName}`
+    },
+    thumbnail: {
+      url: `${iUser.user.avatarURL}`
+    }
+  }
+});
 }
 
 module.exports.help = {
