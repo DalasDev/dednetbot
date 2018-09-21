6f92f7a4c82a48dad5cdc9df3d773db65a306e3d
Alexis Murer <alexis@ottspott.co>
2018-09-21 23:04:45 +0200

    top test

---
+++ commands/top.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@ds239071.mlab.com:39071/retrobotdb");
+var User = require('./../schemas/user_model.js');
+
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_ROLES"))
+    return;
+
+  var topusers = User.find().sort({retrocoinTotal: -1}).limit(3);
+  console.log("Topusers: " + topusers);
+}
+
+module.exports.help = {
+  name: "top"
+}
