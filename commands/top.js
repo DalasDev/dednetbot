06ce3c2d9bc6204a2c3aeb191e83d60c5ef0ab9c
Alexis Murer <alexis@ottspott.co>
2018-09-22 18:23:20 +0200

    scanuser

--- commands/top.js
+++ commands/top.js
@@ -15,25 +15,22 @@ module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_ROLES"))
     return;
-  console.log("DB");
-  var topusers = User.find().sort({retrocoinTotal: -1}).limit(10).lean().exec(function(err, doc) {
+
+  var topusers = User.find().sort({retrocoinTotal: -1}).limit(2).lean().exec(function(err, doc) {
     if(err)
       console.log(err);
     else{
+      var n1 = doc[0].displayName;
+      var n2 = doc[1].displayName;
+      var s1 = doc[0].retrocoinTotal;
+      var s2 = doc[1].retrocoinTotal;
+      console.log("TOP1: "+n1+", score: "+s1+", TOP2: "+n2+", score: "+s2);
       const embed = new Discord.RichEmbed()
       .setTitle("Retro Valley Leaderboard")
       .setColor("#4C8BF5")
       .setThumbnail(retricIcon)
-      .addField(`1) ${doc[0].displayName}`, doc[0].retrocoinTotal, true)
-      .addField(`2) ${doc[1].displayName}`, doc[1].retrocoinTotal, true)
-      .addField(`3) ${doc[2].displayName}`, doc[2].retrocoinTotal, true)
-      .addField(`4) ${doc[3].displayName}`, doc[3].retrocoinTotal, true)
-      .addField(`5) ${doc[4].displayName}`, doc[4].retrocoinTotal, true)
-      .addField(`6) ${doc[5].displayName}`, doc[5].retrocoinTotal, true)
-      .addField(`7) ${doc[6].displayName}`, doc[6].retrocoinTotal, true)
-      .addField(`8) ${doc[7].displayName}`, doc[7].retrocoinTotal, true)
-      .addField(`9) ${doc[8].displayName}`, doc[8].retrocoinTotal, true)
-      .addField(`10) ${doc[9].displayName}`, doc[9].retrocoinTotal, true)
+      .addField(doc[0].displayName, doc[0].retrocoinTotal, true)
+      .addField(doc[1].displayName, doc[1].retrocoinTotal, true)
 
       message.channel.send({embed});
     }
