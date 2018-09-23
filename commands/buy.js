bc42298bb688c3bbe3f8e4b11bc9c7d430f45a05
Alexis Murer <alexis@ottspott.co>
2018-09-23 19:20:29 +0200

    buy test

--- commands/buy.js
+++ commands/buy.js
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
 			else{
 				var itemToBuy = Item.findOne({itemName: args[0]}).lean().exec(function(err, item) {
 					if(err)
-						console.log(err);;
+						console.log(err);
 					else{
 						console.log("User " + user.displayName + " want's to buy a " + item.itemName);
 					}
