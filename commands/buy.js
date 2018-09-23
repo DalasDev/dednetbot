db7a5a84e943050b86e63affd08ae33c87230c8e
Alexis Murer <alexis@ottspott.co>
2018-09-23 19:04:16 +0200

    buy test

--- commands/buy.js
+++ commands/buy.js
@@ -15,10 +15,8 @@ module.exports.run = async (bot, message, args) => {
 	if (isNumeric(args[1]) && !args[2] && Number(args[1]) >= 1) {
 		
 		var user_obj = User.findOne({userID: message.member.id});
-		console.log("user_obj: " + user_obj);
 		var itemToBuy = Item.findOne({itemName: args[0]});
-		console.log("item: " + itemToBuy);
-		console.log("User " + user_obj.displayName + " want's to buy a " + itemToBuy.itemName);
+		console.log("User " + user_obj[0].displayName + " want's to buy a " + itemToBuy[0].itemName);
 	}
 	else
 		return message.reply("чеееее :thinking:");
