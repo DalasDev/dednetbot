7251c0c0064f7f7389cebe06d1165aafa6990d6a
Alexis Murer <alexis@ottspott.co>
2018-09-23 12:50:35 +0200

    test

---
+++ commands/additem.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@ds239071.mlab.com:39071/retrobotdb");
+var Item = require('./../schemas/shop_model.js');
+
+module.exports.run = async (bot, message, args) => {
+
+  //лимит который нужно прописать во все комманды что бы никто другой пока что не использовал
+  if(!message.member.hasPermission("MANAGE_ROLES"))
+    return;
+  if(!message.member.roles.some(r=>["Тех. Администратор", "Губернатор"].includes(r.name)))
+    return;
+  if(!args[4])
+    return message.reply(`name price +/- +/- +/-`);
+  var ifU = (args[2] == '+') ? true : false;
+  var ifS = (args[3] == '+') ? true : false;
+  var ifD = (args[4] == '+') ? true : false;
+  var newItem = new Item({
+    itemName: args[0],
+    itemPrice: Number(args[1]),
+    usable: ifU,
+    sellable: ifS,
+    deletable: ifD,
+    created: Date.now()
+  });
+  newItem.save()
+  .then(item => {
+    console.log('New item "'+args[0]+'" added to database');
+  })
+  .catch(err => {
+    console.log("Error on database save: " + err);
+  });
+}
+
+module.exports.help = {
+  name: "additem"
+}
