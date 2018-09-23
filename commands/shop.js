9e9345e7b8a074d477904622ba840391c30a826c
Alexis Murer <alexis@ottspott.co>
2018-09-23 16:51:37 +0200

    test

--- commands/shop.js
+++ commands/shop.js
@@ -17,36 +17,102 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.roles.some(r=>["Тех. Администратор", "Губернатор", "РетроТестер"].includes(r.name)))
     return;
 
-  var items = Item.find().sort({itemPrice: 1}).limit(10).lean().exec(function(err, doc) {
-    if(err)
-      console.log(err);
-    else{
-      var maxX = doc.length;
-      var x = 0;
-      var y = 0;
-      var text = ``;
-      while (x < maxX){
-        text += `**${y=x+1}.** ${doc[x].itemName} • **${numberWithCommas(doc[x++].itemPrice)} ретриков**\n`;
-      }
-
-      message.channel.send({embed: {
-        color: 3447003,
-        title: `**Retro Valley** :shopping_cart: **Магазин**`,
-        fields: [
-        {
-          name: "(налетай-покупай)",
-          value: text
+  if (!args[0]){
+    var items = Item.find().sort({itemPrice: 1}).limit(10).lean().exec(function(err, doc) {
+      if(err)
+        console.log(err);
+      else{
+        var maxX = doc.length;
+        var x = 0;
+        var y = 0;
+        var text = ``;
+        while (x < maxX){
+          text += `**${y=x+1}.** ${doc[x].itemName} • **${numberWithCommas(doc[x++].itemPrice)} ретриков**\n`;
         }
-        ],
-        timestamp: new Date(),
-        footer: {
-          icon_url: message.author.avatarURL,
-          text: `© ${message.member.displayName}`
-        },
+
+        message.channel.send({embed: {
+          color: 3447003,
+          title: `**Retro Valley** :shopping_cart: **Магазин**`,
+          fields: [
+          {
+            name: "(налетай-покупай)",
+            value: text
+          }
+          ],
+          timestamp: new Date(),
+          footer: {
+            icon_url: message.author.avatarURL,
+            text: `© ${message.member.displayName}`
+          },
+        }
+      });
       }
     });
-    }
-  });
+  }
+  else if (args[0] == '2'){
+    var items = Item.find().sort({itemPrice: 1}).limit(20).lean().exec(function(err, doc) {
+      if(err)
+        console.log(err);
+      else{
+        var maxX = doc.length;
+        var x = 10;
+        var y = 0;
+        var text = ``;
+        while (x < maxX){
+          text += `**${y=x+1}.** ${doc[x].itemName} • **${numberWithCommas(doc[x++].itemPrice)} ретриков**\n`;
+        }
+
+        message.channel.send({embed: {
+          color: 3447003,
+          title: `**Retro Valley** :shopping_cart: **Магазин**`,
+          fields: [
+          {
+            name: "(налетай-покупай)",
+            value: text
+          }
+          ],
+          timestamp: new Date(),
+          footer: {
+            icon_url: message.author.avatarURL,
+            text: `© ${message.member.displayName}`
+          },
+        }
+      });
+      }
+    });
+  }
+  else if (args[0] == '3'){
+    var items = Item.find().sort({itemPrice: 1}).limit(30).lean().exec(function(err, doc) {
+      if(err)
+        console.log(err);
+      else{
+        var maxX = doc.length;
+        var x = 20;
+        var y = 0;
+        var text = ``;
+        while (x < maxX){
+          text += `**${y=x+1}.** ${doc[x].itemName} • **${numberWithCommas(doc[x++].itemPrice)} ретриков**\n`;
+        }
+
+        message.channel.send({embed: {
+          color: 3447003,
+          title: `**Retro Valley** :shopping_cart: **Магазин**`,
+          fields: [
+          {
+            name: "(налетай-покупай)",
+            value: text
+          }
+          ],
+          timestamp: new Date(),
+          footer: {
+            icon_url: message.author.avatarURL,
+            text: `© ${message.member.displayName}`
+          },
+        }
+      });
+      }
+    });
+  }
 }
 
 module.exports.help = {
