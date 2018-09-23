aad6b50ca52a851c1aba0d88a94494c2cb526ba6
Alexis Murer <alexis@ottspott.co>
2018-09-23 18:50:50 +0200

    buy test

---
+++ commands/buy.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@ds239071.mlab.com:39071/retrobotdb");
+var User = require('./../schemas/user_model.js');
+var Item = require('./../schemas/shop_model.js');
+
+function isNumeric(value) {
+	return /^\d+$/.test(value);
+}
+
+module.exports.run = async (bot, message, args) => {
+
+	if (isNumeric(args[1]) && !args[2] && Number(args[1]) >= 1) {
+		
+		var user_obj = User.findOne({userID: message.member.id});
+		var itemToBuy = Item.findOne({itemName: args[0]});
+		console.log("User " + user_obj.displayName + " want's to buy a " + itemToBuy.itemName);
+	}
+	else
+		return message.reply("чеееее :thinking:");
+}
+
+module.exports.help = {
+	name: "buy"
+}
