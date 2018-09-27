f90073ea90e6f133b2a4d0ac5339dcf81385453d
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-27 22:04:32 +0300

    fix

--- schemas/spy_model.js
+++ schemas/spy_model.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var shopSchema = new mongoose.Schema({
+var spySchema = new mongoose.Schema({
 	userName: String,
 	date: Date,
 	message: String,
