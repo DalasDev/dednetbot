0f669fffdcb8c86a29584409466234448eb02fa9
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-09-27 17:43:07 +0300

    fix

--- commands/rouetteinfo.js
+++ commands/rouetteinfo.js
@@ -12,7 +12,9 @@ module.exports.run = async (bot, message, args) => {
 
 	const embed = new Discord.RichEmbed()
 	.setTitle("Информация о рулетке")
+	.setDescription("Как использовать: roulette <ставка> <на что>")
 	.setColor("#BF44FF")
+	.addField("На что можно поставить:", "", true)
 	.setImage("https://retrobotproject.herokuapp.com/images/roulette.jpg")
 
 	message.channel.send({embed});
