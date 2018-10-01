7f1fc352c2c8a1aaec6ba7509b58c31a4b3eddbe
unknown <prostobogdob@gmail.com>
2018-10-01 15:48:10 +0300

    fix

---
+++ commands/changestatus.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@ds239071.mlab.com:39071/retrobotdb");
+var User = require('./../schemas/user_model.js');
+
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.roles.some(r=>["Тех. Администратор", "Губернатор", "РетроТестер"].includes(r.name)))
+    return;
+
+  let status = args.join(" ").slice(22);
+  if (!iUser)
+    return message.reply("укажите статус!");
+
+  var user_obj = User.findOne({
+    userID: message.member.id
+  }, function (err, foundObj) {
+    if (err)
+      console.log("Error on database findOne: " + err);
+    else {
+      if (!foundObj)
+        console.log("Something stange happend");
+      else {
+        foundObj.status = status;
+        foundObj.save(function(err, updatedObj){
+          if(err)
+            console.log(err);
+        })
+      }
+    }
+  });
+}
+
+module.exports.help = {
+  name: "changestatus"
+}
