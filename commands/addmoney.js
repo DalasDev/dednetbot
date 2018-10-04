cc234e15f11a8efb333169c217befd71cdc2e32e
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-10-04 19:14:02 +0300

    addmoney

---
+++ commands/addmoney.js
@@ -0,0 +1,65 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://root:retrobot2018@ds239071.mlab.com:39071/retrobotdb");
+var User = require('./../schemas/user_model.js');
+
+
+function isNumeric(value) {
+	return /^\d+$/.test(value);
+}
+
+function random(min, max) {
+	var result = Math.floor(Math.random() * (max - min + 1)) + min;
+	return (result);
+}
+
+module.exports.run = async (bot, message, args) => {
+
+	var retricIcon = bot.emojis.find("name", "retric");
+	var simpleIcon = bot.emojis.find("name", "this_is_simple");
+  let muser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!muser){
+    message.reply("пользователь не найден / не существует!");
+  }
+  let plase = args[1];
+  let amountin = args[2];
+  if(isNaN(amountin)){
+    message.reply("введите число!");
+  }
+  let amount = Number(amountin);
+
+	var user_obj = User.findOne({
+		userID: muser.id
+	}, function (err, foundObj) {
+		if (err){
+			console.log("Error on database findOne: " + err);
+		}
+		else {
+			if (!foundObj)
+				console.log("Something stange happend");
+			else {
+
+				if(plase == "bank"){
+          foundObj.retrocoinBank = foundObj.retrocoinBank + amount;
+          message.reply(`пользователю <@${muser.id}> добавлено ${amount} ${retric} в банк!`);
+        }else if(plase == "cash"){
+          foundObj.retrocoinCash = foundObj.retrocoinCash + amount;
+          message.reply(`пользователю <@${muser.id}> добавлено ${amount} ${retric} на руки!`);
+        }else{
+          return message.reply(`параметры не верны (^addmoney <пользователь> <bank/cash> <количество>)!`);
+        }
+
+				foundObj.save(function(err, updatedObj){
+				if(err)
+					console.log(err);
+				});
+			}
+		}
+	});
+}
+
+module.exports.help = {
+	name: "addmoney"
+}
