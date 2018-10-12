56c77a345ffa6990d7e96bdabdff68255ab9ae40
Alexis Murer <alexis@ottspott.co>
2018-10-12 22:58:48 +0200

    fix

--- commands/4r.js
+++ commands/4r.js
@@ -4,16 +4,18 @@ module.exports.run = async (bot, message, args) => {
 
   message.delete().catch(O_o=>{});
 
+  var hmmIcon = bot.emojis.find("name", "hmm");
+
   if(!message.member.roles.some(r=>["РетроТестер", "Тех. Администратор", "Губернатор", "⭐Полицейский⭐", "⭐Шерифский департамент⭐"].includes(r.name)))
     return;
 
-  let pnuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if (!pnuser)
+  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if (!user)
   	return;
   let cnchannel = message.guild.channels.find(`name`, "👥черный_рынок");
   let pchannel = message.guild.channels.find(`name`, "📌правила");
 
-  message.channel.send(`${pnuser}, продавать, отдавать или покупать что либо можно только в чате ${cnchannel}! И прочти пожалуйста ${pchannel} :smiley:`)
+  message.channel.send(`${user}, продавать, отдавать или покупать что либо можно только в чате ${cnchannel}! И прочти пожалуйста ${pchannel} ${hmmIcon}`)
 }
 
 module.exports.help = {
--- commands/pn.js
+++ commands/pn.js
@@ -3,17 +3,21 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   message.delete().catch(O_o=>{});
+  
+  var hmmIcon = bot.emojis.find("name", "hmm");
 
   if(!message.member.roles.some(r=>["РетроТестер", "Тех. Администратор", "Губернатор", "⭐Полицейский⭐", "⭐Шерифский департамент⭐"].includes(r.name)))
     return;
 
-  let pnuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if (!user)
+    return; 
   let cnchannel = message.guild.channels.find(`name`, "👥черный_рынок");
   let pchannel = message.guild.channels.find(`name`, "📌правила");
   let kchannel = message.guild.channels.find(`name`, "📵канализация");
   let pnchannel = message.guild.channels.find(`name`, "👋поиск_напарников");
 
-  message.channel.send(`${pnuser}, тебе в ${pnchannel}! И прочти пожалуйста ${pchannel} :smiley:`)
+  message.channel.send(`${user}, тебе в ${pnchannel}! И прочти пожалуйста ${pchannel} ${hmmIcon}`)
 }
 
 module.exports.help = {
