f36ee89091111ae487d45260ce5e2e8f3aabc7f9
Alexis Murer <alexis@ottspott.co>
2018-10-13 12:03:58 +0200

    test

--- commands/pay.js
+++ commands/pay.js
@@ -47,8 +47,8 @@ module.exports.run = async (bot, message, args) => {
 	if (!payed)
 		return message.reply("пользователь не найден / не указан!");
 
-	console.log("payed: " + JSON.stringify(payed))
-	
+	console.log("payed: " + payed);
+
 	if (payed.bot === true)
 		return message.reply("боту твои деньги не нужны, он и так мажор")
 
