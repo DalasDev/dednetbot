fcc9c8a39cd672faff4cf67da9fd7c905c13fbd3
Alexis Murer <alexis@ottspott.co>
2018-10-13 21:09:20 +0200

    test

--- commands/buy.js
+++ commands/buy.js
@@ -39,10 +39,10 @@ module.exports.run = async (bot, message, args) => {
 
 	//проверяю может ли юзер купить то, что задумал
 	if (user_obj.retrocoinCash - item_obj.itemPrice >= 0){
-		console.log("и он может себе это позволить!");
+		return message.channel.send(user_obj.displayName + " пытается купить " + item_obj.itemName);
 	}
 	else{
-		console.log("но у него не хватит на это ретриков");
+		return message.reply("у тебя не хватит на " + item_obj.itemName + " ретриков...");
 	}
 }
 
