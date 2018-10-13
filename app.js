0714eebbda890e1dcaa35fb8c513be12a8e72973
Сергей Рябоконь <macbook@MacBook-Pro-Sergej.local>
2018-10-14 02:52:30 +0300

    fix

--- app.js
+++ app.js
@@ -250,7 +250,7 @@ bot.on("message", async message => {
    && message.content.charAt(3) === "l" && message.content.charAt(4) === "l"){
     let messageArray = message.content.split(" ");
     let cmd = "!sellscan";
-    let var = messageArray.slice(1);
+    var args = messageArray.slice(1);
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
 
     if(commandfile){
