eff57b1efda3e84aafa32f709545f5a63e7a7081
Alexis Murer <alexis@ottspott.co>
2018-10-14 04:35:58 +0200

    bravo

--- commands/molodec.js
+++ commands/molodec.js
@@ -8,7 +8,10 @@ module.exports.run = async (bot, message, args) => {
     	return;
 
 //	.setImage("https://retrobotproject.herokuapp.com/images/bender.gif")
-	message.channel.send("https://retrobotproject.herokuapp.com/images/selffive.gif");
+	const embed = new Discord.RichEmbed()
+	.setImage("https://retrobotproject.herokuapp.com/images/selffive.gif")
+	message.channel.send({embed});
+	//message.channel.send("https://retrobotproject.herokuapp.com/images/selffive.gif");
 }
 
 module.exports.help = {
