12ca316b2570d714364cc8cd48aea50a3d38bb3a
Alexis Murer <alexis@ottspott.co>
2018-10-14 03:21:42 +0200

    fix

--- commands/inv.js
+++ commands/inv.js
@@ -31,7 +31,7 @@ module.exports.run = async (bot, message, args) => {
 
 	let toScan = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-	if (toScan)
+	if (toScan && !message.member.roles.some(r=>["Тех. Администратор", "Губернатор"].includes(r.name)))
 		return message.reply("в чужой карман заглянуть нельзя!");
 
 	if(!toScan){
@@ -59,7 +59,7 @@ module.exports.run = async (bot, message, args) => {
 
 		message.channel.send({embed: {
 			color: randomColor,
-			title: `**${user_obj.displayName}** :zap: **инвентарь**`,
+			title: `**${user_obj.displayName}** :shopping_cart: **инвентарь**`,
 			fields: [
 			{
 				name: "(все что ты успел у нас закупить)",
@@ -73,25 +73,43 @@ module.exports.run = async (bot, message, args) => {
 			},
 		}});
 	} else {
-		console.log("WTF?!");
-		// var user_obj = User.findOne({
-		// 	userID: toScan.id
-		// }, function (err, foundObj) {
-		// 	if (err)
-		// 		console.log("Error on database findOne: " + err);
-		// 	else {
-		// 		var avatar = toScan.user.avatarURL;
-		// 		var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
-		// 		const embed = new Discord.RichEmbed()
-		// 		.setTitle("Личный счет " + toScan.displayName)
-		// 		.setColor("#0000FF")
-		// 		.addField("Наличкой", `${numberWithCommas(foundObj.retrocoinCash)} ${retricIcon}`, true)
-		// 		.addField("В банке", `${numberWithCommas(foundObj.retrocoinBank)} ${retricIcon}`, true)
-		// 		.setThumbnail(avatar)
 
-		// 		message.channel.send({embed});
-		// 	}
-		// });
+		var user_obj = await User.findOne({userID: toScan.id}, function(err, found_user){});
+
+		if (typeof user_obj === 'undefined' || user_obj === null)
+			return message.reply("пользователь не найден в базе");
+
+		var inventory_magic = sort_inv(user_obj.inv);
+		var	inventoryNames = inventory_magic[0];
+		var inventoryCount = inventory_magic[1];
+
+		var maxX = inventoryNames.length;
+		var x = 0;
+		var y = 0;
+		var text = ``;
+
+		while(x < maxX)
+			text += `**${y=x+1}.** ${inventoryNames[x]} • **${numberWithCommas(inventoryCount[x++])}**\n`;
+
+		var min = 0;
+		var max = 16777215;
+		var randomColor = Math.floor(Math.random() * (max - min + 1)) + min;
+
+		message.channel.send({embed: {
+			color: randomColor,
+			title: `**${user_obj.displayName}** :shopping_cart: **инвентарь**`,
+			fields: [
+			{
+				name: "(все что ты успел у нас закупить)",
+				value: text
+			}
+			],
+			timestamp: new Date(),
+			footer: {
+				icon_url: message.author.avatarURL,
+				text: `© ${message.member.displayName}`
+			},
+		}});
 	}
 }
 
