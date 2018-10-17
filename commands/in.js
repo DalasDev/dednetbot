dac45b361eaa34c196677401e32ea23d1b05c95a
Alexis Murer <alexis@ottspott.co>
2018-10-18 01:33:01 +0200

    test

--- commands/in.js
+++ commands/in.js
@@ -5,14 +5,14 @@ module.exports.run = async (bot, message, args) => {
 //  	message.delete().catch(O_o=>{});
 
 	let bicon = bot.user.avatarURL;
-	
+
 	let inembed = new Discord.RichEmbed()
 
 	.setDescription(":sparkling_heart:Приглашение на сервер:sparkling_heart:")
 	.setColor("#b600ff")
 	.setThumbnail(bicon)
 	.addField("https://discord.gg/Ft58PYH", "fire", true)
-	.setFooter("Приглашай своих друганов)", "");
+	.setFooter("Приглашай своих друганов)", "")
 
 	message.channel.send({inembed});
 
