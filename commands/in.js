a95d01a379d123adcaec08b3dfbf028cf5256f3f
Alexis Murer <alexis@ottspott.co>
2018-10-17 23:56:56 +0200

    Semen

---
+++ commands/in.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+//  	message.delete().catch(O_o=>{});
+
+if(cmd === `${prefix}in`){
+
+  let inembed = new Discord.RichEmbed()
+  .setDescription(":sparkling_heart:Приглашение на сервер:sparkling_heart:")
+  .setColor("#b600ff")
+  .addField("https://discord.gg/Ft58PYH")
+  .addField(" ")
+  .addField("Приглашай своих друганов)");
+
+  message.channel.send(inembed);
+  }
+}
+
+module.exports.help = {
+  name: "in"
+}
---
+++ commands/links.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+//  	message.delete().catch(O_o=>{});
+
+if(cmd === `${prefix}links`){
+
+  let linkembed = new Discord.RichEmbed()
+  .setDescription(":fire:Интересные ссылки:fire:")
+  .setColor("#ff7700")
+  .addField("Канал Салливана", "https://www.youtube.com/channel/UCnK6AKESj7cxltJkAsEMsXA")
+  .addField("Группа в ВК", "https://vk.com/sallyshow")
+  .addField("Банда в Steam", "https://steamcommunity.com/groups/sallywan_club");
+  .setimage("https://cdn.discordapp.com/attachments/484779088687661057/502095696724361226/channels4_banner.jpg")
+
+  message.channel.send(linkembed);
+ }
+}
+
+module.exports.help = {
+  name: "links"
+}
