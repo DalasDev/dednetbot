4bf6cb168434adcd43d7d19721c731609e6d221b
Alexis Murer <alexis@ottspott.co>
2018-10-18 01:30:50 +0200

    test

--- commands/in.js
+++ commands/in.js
@@ -4,16 +4,20 @@ module.exports.run = async (bot, message, args) => {
 
 //  	message.delete().catch(O_o=>{});
 
-  let inembed = new Discord.RichEmbed()
-  .setDescription(":sparkling_heart:Приглашение на сервер:sparkling_heart:")
-  .setColor("#b600ff")
-  .addField("https://discord.gg/Ft58PYH", "fire", true)
-  .setFooter("Приглашай своих друганов)", "");
+	let bicon = bot.user.avatarURL;
+	
+	let inembed = new Discord.RichEmbed()
 
-  message.channel.send(inembed);
+	.setDescription(":sparkling_heart:Приглашение на сервер:sparkling_heart:")
+	.setColor("#b600ff")
+	.setThumbnail(bicon)
+	.addField("https://discord.gg/Ft58PYH", "fire", true)
+	.setFooter("Приглашай своих друганов)", "");
+
+	message.channel.send({inembed});
 
 }
 
 module.exports.help = {
-  name: "in"
+	name: "in"
 }
--- commands/links.js
+++ commands/links.js
@@ -4,18 +4,22 @@ module.exports.run = async (bot, message, args) => {
 
 //  	message.delete().catch(O_o=>{});
 
-  let linkembed = new Discord.RichEmbed()
-  .setDescription(":fire:Интересные ссылки:fire:")
-  .setColor("#ff7700")
-  .addField("Канал Салливана", "https://www.youtube.com/channel/UCnK6AKESj7cxltJkAsEMsXA", true)
-  .addField("Группа в ВК", "https://vk.com/sallyshow", true)
-  .addField("Банда в Steam", "https://steamcommunity.com/groups/sallywan_club", true)
-  .setImage("https://cdn.discordapp.com/attachments/484779088687661057/502095696724361226/channels4_banner.jpg");
+	let bicon = bot.user.avatarURL;
 
-  message.channel.send(linkembed);
+	let linkembed = new Discord.RichEmbed()
+
+	.setDescription(":fire:Интересные ссылки:fire:")
+	.setThumbnail(bicon)
+	.setColor("#FF7700")
+	.addField("Канал Салливана", "https://www.youtube.com/channel/UCnK6AKESj7cxltJkAsEMsXA", true)
+	.addField("Группа в ВК", "https://vk.com/sallyshow", true)
+	.addField("Банда в Steam", "https://steamcommunity.com/groups/sallywan_club", true)
+	.setImage("https://cdn.discordapp.com/attachments/484779088687661057/502095696724361226/channels4_banner.jpg");
+
+	message.channel.send({linkembed});
 
 }
 
 module.exports.help = {
-  name: "links"
+	name: "links"
 }
