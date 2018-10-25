7707872308e82d34de9896ab71d4b7ba5c629616
Alexis Murer <alexis@ottspott.co>
2018-10-25 23:34:09 +0200

    fix

--- commands/scanuser.js
+++ commands/scanuser.js
@@ -13,7 +13,7 @@ function getRoles(role, index) {
 
 module.exports.run = async (bot, message) => {
 
-	console.log("Roles: " + message.member.roles.array(role => console.log(role.name)));
+//	console.log("Roles: " + message.member.roles.array(role => console.log(role.name)));
 
 	let r1 = message.guild.roles.find(`name`, "Приезжий [1]");
 	let r2 = message.guild.roles.find(`name`, "Безработный [2]");
