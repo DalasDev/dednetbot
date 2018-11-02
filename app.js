cb6f9138232bb1f55ef708e85ade19eb946e56f1
Alexis Murer <alexis@ottspott.co>
2018-11-02 23:21:53 +0100

    15

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ function idle_repeat(){
   }, null, true, 'Europe/Paris');
 
   let commandfile = bot.commands.get("salariespayement");
-  new CronJob('* 13 * * * *', function() {
+  new CronJob('* 24 * * * *', function() {
     commandfile.run(bot);
   }, null, true, 'Europe/Paris');
 
