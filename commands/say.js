11e92e2ba08287b53f9661583101c20d6988ea51
Dalas <prostobogdob@gmail.com>
2019-12-11 16:27:19 +0200

    Fix

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  let mainchannel = message.guild.channels.find(`name`, "trash ");
+  let mainchannel = message.guild.channels.find(`name`, "trash");
   const sayMessage = args.join(" ");
   message.delete().catch(O_o=>{});
 
