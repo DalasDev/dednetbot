43962b46cfb30abc8c392f48b258d865b0d7e145
Dalas <prostobogdob@gmail.com>
2019-12-26 14:20:58 +0200

    Fix

--- commands/online.js
+++ commands/online.js
@@ -3,17 +3,13 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  let mainchannel = message.guild.channels.find(`name`, "trash");
-  const sayMessage = args.join(" ");
-  message.delete().catch(O_o=>{});
-
   if(!message.member.roles.some(r=>["Discord NPC Developer"].includes(r.name)))
     return;
 
-  message.delete().catch();
-  mainchannel.send(sayMessage);
+  message.delete().catch(O_o=>{});
+  message.channel.send("Я онлайн!");
 }
 
 module.exports.help = {
-  name: "say"
+  name: "online"
 }
