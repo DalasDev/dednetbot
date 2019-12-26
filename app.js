eee8b0d1c20d92a1ff949a065b0cc5acdc7f2f95
Dalas <prostobogdob@gmail.com>
2019-12-26 14:40:26 +0200

    Fix

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ var Spy = require('./schemas/spy_model.js');
 var User = require('./schemas/user_model.js');
 var servers = {};
 var prefix = botconfig.prefix;
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
 
 mongoose.Promise = global.Promise;mongoose.connect(process.env.MONGO_URL);
 
