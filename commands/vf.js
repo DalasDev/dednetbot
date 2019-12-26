fff760b59005aa251a437b78d981df3a64293a6f
Dalas <prostobogdob@gmail.com>
2019-12-26 15:07:54 +0200

    fix

--- commands/vf.js
+++ commands/vf.js
@@ -8,13 +8,14 @@ module.exports.run = async (bot, message, args) => {
   message.delete().catch(O_o=>{});
 
   let server = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  let nickname = args.join(" ").slice(22);
+  let name1 = args[1];
+  let name2 = args.join(" ").slice(22);
   // if(!server)
   //   return message.reply("Команда введена неверно! Пример: !vf [Имя] [Сервер]");
   // if(!nickname)
   //   return message.reply("Команда введена неверно! Пример: !vf [Имя] [Сервер]");
 
-    message.channel.send(server+" "+nickname);
+    message.channel.send(server+" "+name1+" "+name2);
 
 }
 
