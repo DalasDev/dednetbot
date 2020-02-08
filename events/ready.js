c6164add2085b70ce081c27e4ae2e4fbfe5febdf
Dalas <prostobogdob@gmail.com>
2020-02-08 16:16:04 +0200

    Fix

--- events/ready.js
+++ events/ready.js
@@ -7,16 +7,16 @@ const ready = class extends Event {
   }
 
   run() {
-    // this.client.db.connection.collection('users').insertMany(this.client.guilds.get('633421720572919838').members.map(m => ({
-    //     id: m.id,
-    //     username: m.displayName,
-    //     invitelink: undefined,
-    //     invitecount: 0,
-    //     invitedbyid: undefined,
-    // })), (err, res) => {
-    //   if (err) return console.error(err);
-    //   console.log('Юзеры добавлены');
-    // });
+    this.client.db.connection.collection('users').insertMany(this.client.guilds.get('633421720572919838').members.map(m => ({
+        id: m.id,
+        username: m.displayName,
+        invitelink: undefined,
+        invitecount: 0,
+        invitedbyid: undefined,
+    })), (err, res) => {
+      if (err) return console.error(err);
+      console.log('Юзеры добавлены');
+    });
 
     //Консоль лог что бот онлайн
     console.log(`[app.js] ${this.client.user.username} онлайн`);
