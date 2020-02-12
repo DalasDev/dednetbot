f081614efea02f4e8590e7c8b9799fc868e838e7
Dalas <prostobogdob@gmail.com>
2020-02-12 18:27:32 +0200

    Fix

--- events/invitemanager.js
+++ events/invitemanager.js
@@ -13,7 +13,7 @@ const memberAdd = class extends Event {
 
   run(member) {
     const col = this.client.db.getCollection('users');
-    if(this.client.db.getCollection('users').findOne(d => d.id === member.user.id)){
+    if(this.client.db.getCollection('users').findOne(d => d.id === member.id)){
         return console.log("Инвайт не засчитан так как человек есть в дб");
     }
     member.guild.fetchInvites().then(invites => {
