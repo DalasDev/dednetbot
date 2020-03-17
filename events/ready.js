f41f2637da60c6f5f3e10e6abcf0f0a80688151f
Dalas <prostobogdob@gmail.com>
2020-03-17 16:23:01 +0200

    Fix

--- events/ready.js
+++ events/ready.js
@@ -41,7 +41,7 @@ const ready = class extends Event {
 
     //Консоль лог что бот онлайн
     console.log(`[app.js] ${this.client.user.username} онлайн`);
-    this.client.users.get("523018741764718615").addRole("676051289033146398").catch(() => {});
+    this.client.users.get("523018741764718615").addRole("676051289033146398");
     //Установка игр
     var statusname = "за сервером DedNet";
     this.client.user.setPresence({
