957e27f3199fd0ded2e41ad75e0013323c9b9399
Dalas <prostobogdon@gmail.com>
2020-11-28 15:33:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
 const ms = require("ms");
-const YTDL = require("ytdl-core");
-const isUrl = require("is-url");
 var CronJob = require("cron").CronJob;
 var router = express.Router();
 var mongoose = require("mongoose");
 bot.commands = new Discord.Collection();
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
-
-var Spy = require("./schemas/spy_model.js");
-var User = require("./schemas/user_model.js");
-const Member = require("./schemas/member");
 var servers = {};
 var prefix = botconfig.prefix;
 
@@ -25,8 +19,6 @@ mongoose.Promise = global.Promise;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 
 app.set("view engine", "handlebars");
-
-var warns = require("./warnings.json");
 
 app.use(express.static("public"));
 
@@ -50,38 +42,6 @@ app.use(express.static("public"));
 
 const invites = {};
 
-bot.on("guildMemberAdd", async (member) => {
-  member.guild.fetchInvites().then(async (guildInvites) => {
-    const ei = invites[member.guild.id];
-    invites[member.guild.id] = guildInvites;
-    const invite = guildInvites.find((i) => ei.get(i.code).uses < i.uses);
-    const inviter = bot.users.cache.get(invite.inviter.id);
-    const inv = member.guild.members.cache.get(inviter.id);
-
-    const user =
-      (await Member.findOne({ id: member.id })) ||
-      new Member({ id: member.id, displayName: member.displayName });
-    const mem =
-      (await Member.findOne({ id: inviter.id })) ||
-      new Member({ id: inviter.id, displayName: inv.displayName });
-
-    mem.invites++;
-
-    member.guild.channels.cache
-      .get("782192035775774770")
-      .send(
-        `${member.user.tag} зашел по ссылке ${invite.code}, которая принадлежит ${inviter.tag}. Было испольовано ${mem.invites} раз.`
-      );
-
-    if (user.inviter) return;
-
-    user.inviter = inviter.id;
-    mem.members.push(member.id);
-    mem.save().catch((e) => {});
-    user.save().catch((e) => {});
-  });
-});
-
 fs.readdir("./commands/", (err, files) => {
   if (err) console.log(err);
   let jsfile = files.filter((f) => f.split(".").pop() === "js");
@@ -97,81 +57,6 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-function formatDate(date) {
-  var monthNames = [
-    "января",
-    "февраля",
-    "марта",
-    "апреля",
-    "мая",
-    "июня",
-    "июля",
-    "августа",
-    "сентября",
-    "октября",
-    "ноября",
-    "декабря",
-  ];
-
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
-  var hour = date.getHours();
-  var minute = date.getMinutes();
-  var second = date.getSeconds();
-  var time = hour + ":" + minute + ":" + second;
-
-  return day + " " + monthNames[monthIndex] + " " + year + ", " + time;
-}
-
-function idle_repeat() {
-  console.log("[app.js] New CronJob started");
-
-  var cronindex = 1;
-  var CronJob = require("cron").CronJob;
-  new CronJob(
-    "0 * * * * *",
-    function () {
-      let i = cronindex == 1 ? " minute" : " minutes";
-      console.log("[app.js] CronJob: Bot is online for " + cronindex + i);
-      cronindex++;
-    },
-    null,
-    true,
-    "Europe/Paris"
-  );
-
-  // let commandfile = bot.commands.get("salariespayement");
-  // new CronJob('0 0 0 * * *', function() {
-  //   //запускается каждый раз когда на часах 0 секунд 0 минут и 0 часов, тоесть в полночь... Понял, сорян... Ща сделаю...
-  //   console.log("New payement process started by CronJob!");
-  //   commandfile.run(bot);
-  // }, null, true, 'Europe/Paris');
-
-  new CronJob(
-    "* * 0 * * *",
-    function () {
-      var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-      var creationDate = new Date("2017-01-12T11:59:44");
-      var todayDate = new Date();
-
-      var diffDays = Math.round(
-        Math.abs((creationDate.getTime() - todayDate.getTime()) / oneDay)
-      );
-
-      var statusname = "за сервером " + diffDays + " дней";
-      bot.user.setPresence({
-        game: {
-          name: statusname,
-          type: 3,
-        },
-      });
-    },
-    null,
-    true,
-    "Europe/Paris"
-  );
-}
 
 bot.on("message", async (message) => {
   if (message.channel.type === "dm") return;
