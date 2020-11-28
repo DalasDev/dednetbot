d4c754aeea9514144eace91819e77474a468db9f
DJS3M3N <spiryaev.semen@mail.ru>
2020-11-28 19:31:00 +0400

    fix leave

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ bot.on("message", async (message) => {
 
 bot.on("guildMemberRemove", (member) => {
   member.guild.channels.cache
-    .get("782192035775774770")
+    .get("782165776630939649")
     .send(`${member.user.tag} ливнул с сервера`);
 });
 //message.author.id == '363730744553766913' || message.author.id == '381457099789565953'
