72a2287e3123edb641eb938376522f8d38d95da6
Dalas <prostobogdon@gmail.com>
2020-11-28 15:46:58 +0200

    Update pizda.js

--- commands/pizda.js
+++ commands/pizda.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.roles.some(r=>["Fredbot"].includes(r.name)))
+  if(!message.member.roles.cache.some(r=>["Fredbot"].includes(r.name)))
     return;
 
   let x = 0;
